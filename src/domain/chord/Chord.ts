81d0c1f65a01b53500378d9ff2ec687726c2908f
Ryosuke Takahashi <rjooske@gmail.com>
2022-05-26 00:56:25 +0900

    Chord class that can check equality

---
+++ src/domain/chord/Chord.ts
@@ -0,0 +1,29 @@
+import { Key } from "./Key";
+
+export class Chord {
+  private readonly keys: Key[];
+
+  constructor(keys: Key[]) {
+    this.keys = dedupe(keys);
+  }
+
+  equals(chord: Chord) {
+    if (this.keys.length !== chord.keys.length) {
+      return false;
+    }
+
+    return this.keys.every((key) => chord.keys.includes(key));
+  }
+}
+
+function dedupe(keys: Key[]) {
+  const result: Key[] = [];
+
+  for (const key of keys) {
+    if (!result.includes(key)) {
+      result.push(key);
+    }
+  }
+
+  return result;
+}
